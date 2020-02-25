23a6d93c6b345939117191660cfd1510b743cbe9
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-24 21:42:44 -0300

    🔧  config: axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, Button } from 'react-native';
+
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-const Main = ({ navigation }) => {
-  return (
-    <>
-      <Text> Hmm ... Hellow </Text>
-      <Button title="Cart" onPress={() => navigation.navigate('Cart')} />
-    </>
-  );
-};
+class Main extends Component {
+  async componentDidMount() {
+    await api.get('/products');
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <>
+        <Text> Hmm ... Hellow </Text>
+        <Button title="Cart" onPress={() => navigation.navigate('Cart')} />
+      </>
+    );
+  }
+}
 
 export default Main;
