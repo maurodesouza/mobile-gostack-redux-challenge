cdc52b59b315516f31f7055cb8913af336ad7f21
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-25 23:42:13 -0300

    ⚡  improve: reactotron with redux

--- src/config/reactotronConfig.js
+++ src/config/reactotronConfig.js
@@ -1,10 +1,12 @@
 import reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = reactotron
     .configure()
     .useReactNative()
+    .use(reactotronRedux())
     .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
